fc6064d385f0227384965201bffc459685e57f90
Poga Po <poga.bahamut@gmail.com>
2017-03-22 13:26:51 +0800

    rewrite with hypercore v5

--- bin.js
+++ bin.js
@@ -1,51 +1,23 @@
-const hyperdrive = require('hyperdrive')
-const level = require('level')
 const argv = require('minimist')(process.argv.slice(2))
 const ReplayDB = require('.')
-const fs = require('fs')
 const mkdirp = require('mkdirp')
-const path = require('path')
-const raf = require('random-access-file')
+const swarm = require('hyperdiscovery')
 
 const dbPath = argv._[0]
-const key = argv._[1]
 
 if (!dbPath) throw new Error('path is required')
 
-try {
-  if (!fs.statSync(dbPath).isDirectory()) throw new Error('path is not a dir')
-  if (!key) {
-    console.log('key is required if dir exist')
-  } else {
-    // path exists, open existing archive in the path
-    var archive = createArchive(dbPath, key)
-    console.log('archive opened', archive.key.toString('hex'))
-    run(archive)
-  }
-} catch (e) {
-  mkdirp(dbPath, err => {
-    if (err) throw err
-
-    var archive = createArchive(dbPath, key)
-    console.log('new archive created', archive.key.toString('hex'))
-    run(archive)
+mkdirp(dbPath, function (err) {
+  if (err) throw err
+  var db = new ReplayDB(dbPath)
+  var app = db.server()
+  var port = argv.port || 8080
+  db.feed.on('ready', () => {
+    console.log('key', db.feed.key.toString('hex'))
+    swarm(db.feed, {live: true})
   })
-}
-
-function run (archive) {
-  archive.open(() => {
-    var db = new ReplayDB(archive)
-    var port = argv.port || 8080
-    db.listen(port, () => {
-      console.log('listening at', port)
-    })
+  db.feed.on('upload', (i, d) => { console.log('uploaded', i, d.length) })
+  app.listen(port, () => {
+    console.log('listening at', port)
   })
-}
-
-function createArchive (dir, key) {
-  console.log('creating', path.join(dir, '.replaydb'))
-  var drive = hyperdrive(level(path.join(dir, '.replaydb')))
-  return drive.createArchive(key, {
-    file: name => raf(path.join(dir, name))
-  })
-}
+})
--- index.js
+++ index.js
@@ -1,18 +1,59 @@
-const server = require('./server')
+const hypercore = require('hypercore')
+const _ = require('lodash')
+const express = require('express')
+const bodyParser = require('body-parser')
+const events = require('events')
+const inherits = require('inherits')
 
-function ReplayDB (archive) {
-  if (!(this instanceof ReplayDB)) return new ReplayDB(archive)
-  this.server = server(archive)
+function ReplayDB (path) {
+  if (!(this instanceof ReplayDB)) return new ReplayDB(path)
+  events.EventEmitter.call(this)
+
+  this.path = path
+  this.feed = hypercore(path)
+  this.buffer = new Buffer(0)
+  this.flush = _.debounce(this.flushNow, 1000, {maxWait: 5000})
 }
 
-ReplayDB.prototype.listen = function (port, cb) {
-  this.app = this.server.listen(port, cb)
+inherits(ReplayDB, events.EventEmitter)
+
+ReplayDB.prototype.append = function (object) {
+  this.buffer = Buffer.concat([this.buffer, new Buffer(JSON.stringify(object) + '\n')])
+  this.flush()
 }
 
-ReplayDB.prototype.close = function (cb) {
-  if (!this.app) return cb(new Error('nothing to close'))
+ReplayDB.prototype.flushNow = function () {
+  console.log(this.buffer.toString().split('\n').length, 'flushed')
+  var temp = Buffer.from(this.buffer)
+  this.buffer = new Buffer(0)
+  this.feed.append(temp, (err) => {
+    if (err) throw err
+    console.log('append done')
+  })
+}
 
-  this.app.close(cb)
+ReplayDB.prototype.server = function (cb) {
+  var app = this.app = express()
+  var self = this
+  require('express-ws')(app)
+
+  app.use(bodyParser.json())
+
+  app.post('/:topic', function (req, res) {
+    self.append(req.body)
+    res.json({status: 'ok'})
+  })
+
+  app.ws('/live/:topic', function (ws, res) {
+
+  })
+
+  app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something went wrong')
+  })
+
+  return app
 }
 
 module.exports = ReplayDB
