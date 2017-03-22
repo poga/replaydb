43bde250d3514d8631c39ef4b8c4b695fc662bc9
Poga Po <poga.bahamut@gmail.com>
2017-03-22 20:30:03 +0800

    add tests for append

---
+++ test/append.js
@@ -0,0 +1,66 @@
+const tape = require('tape')
+const DB = require('..')
+const tmp = require('tmp')
+const replicate = require('./helpers/replicate')
+
+tape('append 1 flush 1', function (t) {
+  var dir = tmp.dirSync({unsafeCleanup: true})
+  var db = new DB(dir.name)
+  db.open(test)
+
+  function test () {
+    db.append({foo: 'bar'})
+    db.on('flush', buf => {
+      t.same(JSON.parse(buf), {foo: 'bar'})
+      t.end()
+    })
+  }
+})
+
+tape('append n flush 1', function (t) {
+  var dir = tmp.dirSync({unsafeCleanup: true})
+  var db = new DB(dir.name)
+  db.open(test)
+
+  const n = 5
+
+  function test () {
+    for (var i = 0; i < n; i++) {
+      db.append({foo: 'bar' + i})
+    }
+    db.on('flush', buf => {
+      var objects = buf.toString().trim().split('\n').map(x => JSON.parse(x))
+      t.same(objects, [
+        {foo: 'bar0'},
+        {foo: 'bar1'},
+        {foo: 'bar2'},
+        {foo: 'bar3'},
+        {foo: 'bar4'}
+      ])
+      t.end()
+    })
+  }
+})
+
+tape('append 2 flush 2', function (t) {
+  var dir = tmp.dirSync({unsafeCleanup: true})
+  var db = new DB(dir.name)
+  db.open(test)
+
+  function test () {
+    db.append({foo: 'bar'})
+    setTimeout(function () {
+      db.append({foo: 'baz'})
+    }, 1500)
+    var iter = 0
+    db.on('flush', buf => {
+      if (iter === 0) {
+        t.same(JSON.parse(buf), {foo: 'bar'})
+        iter++
+      } else {
+        t.same(JSON.parse(buf), {foo: 'baz'})
+        t.end()
+      }
+    })
+  }
+})
